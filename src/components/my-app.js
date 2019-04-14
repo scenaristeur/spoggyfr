7acbfb8f2fae7fab60a0e0b67272e3936a6a8380
unknown <scenaristeur@gmail.com>
2019-04-14 11:50:07 +0200

    add network png

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -37,145 +37,145 @@ class MyApp extends LitElement {
   static get styles() {
     return [
       css`
-        :host {
-          display: block;
+      :host {
+        display: block;
 
-          --app-drawer-width: 256px;
+        --app-drawer-width: 256px;
 
-          --app-primary-color: #E91E63;
-          --app-secondary-color: #293237;
-          --app-dark-text-color: var(--app-secondary-color);
-          --app-light-text-color: white;
-          --app-section-even-color: #f7f7f7;
-          --app-section-odd-color: white;
+        --app-primary-color: #E91E63;
+        --app-secondary-color: #293237;
+        --app-dark-text-color: var(--app-secondary-color);
+        --app-light-text-color: white;
+        --app-section-even-color: #f7f7f7;
+        --app-section-odd-color: white;
 
-          --app-header-background-color: white;
-          --app-header-text-color: var(--app-dark-text-color);
-          --app-header-selected-color: var(--app-primary-color);
+        --app-header-background-color: white;
+        --app-header-text-color: var(--app-dark-text-color);
+        --app-header-selected-color: var(--app-primary-color);
 
-          --app-drawer-background-color: var(--app-secondary-color);
-          --app-drawer-text-color: var(--app-light-text-color);
-          --app-drawer-selected-color: #78909C;
-        }
+        --app-drawer-background-color: var(--app-secondary-color);
+        --app-drawer-text-color: var(--app-light-text-color);
+        --app-drawer-selected-color: #78909C;
+      }
 
-        app-header {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          text-align: center;
-          background-color: var(--app-header-background-color);
-          color: var(--app-header-text-color);
-          border-bottom: 1px solid #eee;
-        }
+      app-header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        text-align: center;
+        background-color: var(--app-header-background-color);
+        color: var(--app-header-text-color);
+        border-bottom: 1px solid #eee;
+      }
 
-        .toolbar-top {
-          background-color: var(--app-header-background-color);
-        }
+      .toolbar-top {
+        background-color: var(--app-header-background-color);
+      }
 
-        [main-title] {
-          font-family: 'Pacifico';
-          text-transform: lowercase;
-          font-size: 30px;
-          /* In the narrow layout, the toolbar is offset by the width of the
-          drawer button, and the text looks not centered. Add a padding to
-          match that button */
-          padding-right: 44px;
-        }
+      [main-title] {
+        font-family: 'Pacifico';
+        text-transform: lowercase;
+        font-size: 30px;
+        /* In the narrow layout, the toolbar is offset by the width of the
+        drawer button, and the text looks not centered. Add a padding to
+        match that button */
+        padding-right: 44px;
+      }
 
+      .toolbar-list {
+        display: none;
+      }
+
+      .toolbar-list > a {
+        display: inline-block;
+        color: var(--app-header-text-color);
+        text-decoration: none;
+        line-height: 30px;
+        padding: 4px 24px;
+      }
+
+      .toolbar-list > a[selected] {
+        color: var(--app-header-selected-color);
+        border-bottom: 4px solid var(--app-header-selected-color);
+      }
+
+      .menu-btn {
+        background: none;
+        border: none;
+        fill: var(--app-header-text-color);
+        cursor: pointer;
+        height: 44px;
+        width: 44px;
+      }
+
+      .drawer-list {
+        box-sizing: border-box;
+        width: 100%;
+        height: 100%;
+        padding: 24px;
+        background: var(--app-drawer-background-color);
+        position: relative;
+      }
+
+      .drawer-list > a {
+        display: block;
+        text-decoration: none;
+        color: var(--app-drawer-text-color);
+        line-height: 40px;
+        padding: 0 24px;
+      }
+
+      .drawer-list > a[selected] {
+        color: var(--app-drawer-selected-color);
+      }
+
+      /* Workaround for IE11 displaying <main> as inline */
+      main {
+        display: block;
+      }
+
+      .main-content {
+        padding-top: 64px;
+        min-height: 100vh;
+      }
+
+      .page {
+        display: none;
+      }
+
+      .page[active] {
+        display: block;
+      }
+
+      footer {
+        padding: 24px;
+        background: var(--app-drawer-background-color);
+        color: var(--app-drawer-text-color);
+        text-align: center;
+      }
+
+      /* Wide layout: when the viewport width is bigger than 460px, layout
+      changes to a wide layout */
+      @media (min-width: 460px) {
         .toolbar-list {
-          display: none;
-        }
-
-        .toolbar-list > a {
-          display: inline-block;
-          color: var(--app-header-text-color);
-          text-decoration: none;
-          line-height: 30px;
-          padding: 4px 24px;
-        }
-
-        .toolbar-list > a[selected] {
-          color: var(--app-header-selected-color);
-          border-bottom: 4px solid var(--app-header-selected-color);
+          display: block;
         }
 
         .menu-btn {
-          background: none;
-          border: none;
-          fill: var(--app-header-text-color);
-          cursor: pointer;
-          height: 44px;
-          width: 44px;
-        }
-
-        .drawer-list {
-          box-sizing: border-box;
-          width: 100%;
-          height: 100%;
-          padding: 24px;
-          background: var(--app-drawer-background-color);
-          position: relative;
-        }
-
-        .drawer-list > a {
-          display: block;
-          text-decoration: none;
-          color: var(--app-drawer-text-color);
-          line-height: 40px;
-          padding: 0 24px;
-        }
-
-        .drawer-list > a[selected] {
-          color: var(--app-drawer-selected-color);
-        }
-
-        /* Workaround for IE11 displaying <main> as inline */
-        main {
-          display: block;
-        }
-
-        .main-content {
-          padding-top: 64px;
-          min-height: 100vh;
-        }
-
-        .page {
           display: none;
         }
 
-        .page[active] {
-          display: block;
+        .main-content {
+          padding-top: 107px;
         }
 
-        footer {
-          padding: 24px;
-          background: var(--app-drawer-background-color);
-          color: var(--app-drawer-text-color);
-          text-align: center;
+        /* The drawer button isn't shown in the wide layout, so we don't
+        need to offset the title */
+        [main-title] {
+          padding-right: 0px;
         }
-
-        /* Wide layout: when the viewport width is bigger than 460px, layout
-        changes to a wide layout */
-        @media (min-width: 460px) {
-          .toolbar-list {
-            display: block;
-          }
-
-          .menu-btn {
-            display: none;
-          }
-
-          .main-content {
-            padding-top: 107px;
-          }
-
-          /* The drawer button isn't shown in the wide layout, so we don't
-          need to offset the title */
-          [main-title] {
-            padding-right: 0px;
-          }
-        }
+      }
       `
     ];
   }
@@ -183,47 +183,53 @@ class MyApp extends LitElement {
   render() {
     // Anything that's related to rendering should be done in here.
     return html`
-      <!-- Header -->
-      <app-header condenses reveals effects="waterfall">
-        <app-toolbar class="toolbar-top">
-          <button class="menu-btn" title="Menu" @click="${this._menuButtonClicked}">${menuIcon}</button>
-          <div main-title>${this.appTitle}</div>
-        </app-toolbar>
+    <!-- Header -->
+    <app-header condenses reveals effects="waterfall">
+    <app-toolbar class="toolbar-top">
+    <button class="menu-btn" title="Menu" @click="${this._menuButtonClicked}">${menuIcon}</button>
+    <div main-title>${this.appTitle}</div>
+    </app-toolbar>
 
-        <!-- This gets hidden on a small screen-->
-        <nav class="toolbar-list">
-          <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
-          <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
-          <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
-        </nav>
-      </app-header>
+    <!-- This gets hidden on a small screen-->
+    <nav class="toolbar-list">
+    <a ?selected="${this._page === 'view1'}" href="/view1">Accueil</a>
+    <a ?selected="${this._page === 'view2'}" href="/view2">Sarl Api</a>
+    <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
+    <a ?selected="${this._page === 'view4'}" href="/view4">Graphe</a>
+    <a ?selected="${this._page === 'view5'}" href="/view5">Mon compte</a>
+    </nav>
+    </app-header>
 
-      <!-- Drawer content -->
-      <app-drawer
-          .opened="${this._drawerOpened}"
-          @opened-changed="${this._drawerOpenedChanged}">
-        <nav class="drawer-list">
-          <a ?selected="${this._page === 'view1'}" href="/view1">View One</a>
-          <a ?selected="${this._page === 'view2'}" href="/view2">View Two</a>
-          <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
-        </nav>
-      </app-drawer>
+    <!-- Drawer content -->
+    <app-drawer
+    .opened="${this._drawerOpened}"
+    @opened-changed="${this._drawerOpenedChanged}">
+    <nav class="drawer-list">
+    <a ?selected="${this._page === 'view1'}" href="/view1">Accueil</a>
+    <a ?selected="${this._page === 'view2'}" href="/view2">Sarl Api</a>
+    <a ?selected="${this._page === 'view3'}" href="/view3">View Three</a>
+    <a ?selected="${this._page === 'view4'}" href="/view4">Graphe</a>
+    <a ?selected="${this._page === 'view5'}" href="/view5">Mon compte</a>
+    </nav>
+    </app-drawer>
 
-      <!-- Main content -->
-      <main role="main" class="main-content">
-        <my-view1 class="page" ?active="${this._page === 'view1'}"></my-view1>
-        <my-view2 class="page" ?active="${this._page === 'view2'}"></my-view2>
-        <my-view3 class="page" ?active="${this._page === 'view3'}"></my-view3>
-        <my-view404 class="page" ?active="${this._page === 'view404'}"></my-view404>
-      </main>
+    <!-- Main content -->
+    <main role="main" class="main-content">
+    <my-view1 class="page" ?active="${this._page === 'view1'}"></my-view1>
+    <my-view2 class="page" ?active="${this._page === 'view2'}"></my-view2>
+    <my-view3 class="page" ?active="${this._page === 'view3'}"></my-view3>
+    <my-view4 class="page" ?active="${this._page === 'view4'}">Le premier chargement du graphe peut être long...</my-view4>
+    <my-view5 class="page" ?active="${this._page === 'view5'}"></my-view5>
+    <my-view404 class="page" ?active="${this._page === 'view404'}"></my-view404>
+    </main>
 
-      <footer>
-        <p>Made with &hearts; by the Polymer team.</p>
-      </footer>
+    <footer>
+    <p>Made with &hearts; by the Polymer team.</p>
+    </footer>
 
-      <snack-bar ?active="${this._snackbarOpened}">
-        You are now ${this._offline ? 'offline' : 'online'}.
-      </snack-bar>
+    <snack-bar ?active="${this._snackbarOpened}">
+    You are now ${this._offline ? 'offline' : 'online'}.
+    </snack-bar>
     `;
   }
 
@@ -239,7 +245,7 @@ class MyApp extends LitElement {
     installRouter((location) => this._locationChanged(location));
     installOfflineWatcher((offline) => this._offlineChanged(offline));
     installMediaQueryWatcher(`(min-width: 460px)`,
-        (matches) => this._layoutChanged(matches));
+    (matches) => this._layoutChanged(matches));
   }
 
   updated(changedProps) {
@@ -292,20 +298,26 @@ class MyApp extends LitElement {
   _loadPage(page) {
     switch(page) {
       case 'view1':
-        import('../components/my-view1.js').then((module) => {
-          // Put code in here that you want to run every time when
-          // navigating to view1 after my-view1.js is loaded.
-        });
-        break;
+      import('../components/my-view1.js').then((module) => {
+        // Put code in here that you want to run every time when
+        // navigating to view1 after my-view1.js is loaded.
+      });
+      break;
       case 'view2':
-        import('../components/my-view2.js');
-        break;
+      import('../components/my-view2.js');
+      break;
       case 'view3':
-        import('../components/my-view3.js');
-        break;
+      import('../components/my-view3.js');
+      break;
+      case 'view4':
+      import('../components/my-view4.js');
+      break;
+      case 'view5':
+      import('../components/my-view5.js');
+      break;
       default:
-        page = 'view404';
-        import('../components/my-view404.js');
+      page = 'view404';
+      import('../components/my-view404.js');
     }
 
     this._page = page;
---
+++ src/components/my-view4.js
@@ -0,0 +1,34 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+import { html } from 'lit-element';
+import { PageViewElement } from './page-view-element.js';
+
+// These are the shared styles needed by this element.
+import { SharedStyles } from './shared-styles.js';
+import  './graphe/solid-graph.js';
+
+class MyView4 extends PageViewElement {
+  static get styles() {
+    return [
+      SharedStyles
+    ];
+  }
+
+  render() {
+    return html`
+    <section>
+    <solid-graph></solid-graph>
+    </section>
+    `;
+  }
+}
+
+window.customElements.define('my-view4', MyView4);
