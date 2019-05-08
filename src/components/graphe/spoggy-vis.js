ece5b99d7728e1185d8f769d2fa924553d3fa9e8
unknown <scenaristeur@gmail.com>
2019-05-08 15:52:57 +0200

    restore pwa-helpers

--- src/components/graphe/spoggy-vis.js
+++ src/components/graphe/spoggy-vis.js
@@ -198,11 +198,11 @@ class SpoggyVis extends LitElement {
             var current = {}
             current.url = existNode[0].id;
             current.type = existNode[0].type;
-            app.agentVis.send('agentFoldermenu', {type: "currentChanged", current: current});
-            app.agentVis.send('agentCurrent', {type: "currentChanged", current: current});
-          //  app.agentVis.send('agentGraph', {type: "currentChanged", current: current});
-            app.agentVis.send('agentFileeditor', {type: "currentChanged", current: current});
-            app.agentVis.send('agentIde', {type: 'currentChanged', current: current });
+          //  app.agentVis.send('agentFoldermenu', {type: "currentChanged", current: current});
+          //  app.agentVis.send('agentCurrent', {type: "currentChanged", current: current});
+            app.agentVis.send('agentGraph', {type: "currentChanged", current: current});
+          //  app.agentVis.send('agentFileeditor', {type: "currentChanged", current: current});
+          //  app.agentVis.send('agentIde', {type: 'currentChanged', current: current });
             //  app.agentVis.send('agentFoldermenu', {type: "nodeChanged", node: existNode[0]});
             //  network.body.data.nodes.add(data);
             //  var thing = this.thing;
